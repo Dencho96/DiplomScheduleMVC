9354231a60dcef3c1ad3f39c6690d75e762722b0
serey <serey@DESKTOP-SK2JDQ9>
2018-06-21 23:25:27 +0300

    auth

---
+++ DiplomScheduleMVC/Controllers/AccountController.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DiplomScheduleMVC.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public AccountController()
+        {
+        }
+
+        #region Login
+
+        [HttpGet]
+        [AllowAnonymous]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<ActionResult> Login(string username, string password)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+            if (username == "admin"&&password=="123")
+            {
+                var identity = new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.Name, username),
+                    },
+                    "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect("/Home/Index");
+            }
+            else
+            {
+                ViewBag.InvalidParameters = "Check";
+                return View();
+            }
+
+            //ModelState.AddModelError("", "Invalid username or password");
+            //return View();
+        }
+
+        #endregion
+
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "home");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+            return returnUrl;
+        }
+    }
+
+}
